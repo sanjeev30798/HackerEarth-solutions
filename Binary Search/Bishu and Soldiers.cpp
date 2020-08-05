4bc3e979f25ab19328e13fa1d9ef9dbfd0dd56c9
Sanjeev Kumar <49332829+sanjeev30798@users.noreply.github.com>
2020-08-05 10:21:35 +0530

    Add files via upload

---
+++ Binary Search/Bishu and Soldiers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int q;
+    cin>>q;
+    while(q--)
+    {
+        int p,i,sum=0,count=0;
+        cin>>p;
+        for(i=0;i<n;i++)
+        {
+            if(p>=a[i])
+            {
+                count++;
+                sum+=a[i];
+            }
+        }
+        cout<<count<<" "<<sum<<endl;
+    }
+	return 0;
+}
